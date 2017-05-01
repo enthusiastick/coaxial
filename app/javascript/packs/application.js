a2c2370c30518092d8542659b5471f145ce74aba
Eben <eben.lowe@gmail.com>
2017-05-01 15:53:46 -0400

    Implement react-router.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,21 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import 'whatwg-fetch';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
 
-console.log('Hello World from Webpacker')
+import HelloReact from './hello_react';
+
+const history = createBrowserHistory();
+
+document.addEventListener('DOMContentLoaded', () => {
+  let reactElement = document.getElementById('react-app');
+  if (reactElement) {
+    ReactDOM.render(
+       <Router history={history}>
+        <Route path='/' component={HelloReact} />
+       </Router>,
+      reactElement
+    );
+  }
+})
